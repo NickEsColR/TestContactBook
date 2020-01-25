bdb58abde49e7db9de6d2d312745b84116ecf202
user.name <nicoescol0818@gmail.com>
2020-01-25 18:01:54 -0500

    falta metodo remove clase ContactBook

--- TestContactBook/src/model/Contact.java
+++ TestContactBook/src/model/Contact.java
@@ -1,5 +1,32 @@
 package model;
 
 public class Contact {
+	
+	//attributtes
+	
+	private String name;
+	private String email;
+	private String phone;
+	
+	//methods
+	
+	public Contact(String n, String e, String p) {
+		super();
+		name = n;
+		email = e;
+		phone = p;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+	
 }
--- TestContactBook/src/model/ContactBook.java
+++ TestContactBook/src/model/ContactBook.java
@@ -1,5 +1,40 @@
 package model;
-
+import java.util.ArrayList;
 public class ContactBook {
+	
+	//relations
+	
+	private ArrayList<Contact> contacts;
+	
+	//methods
+	
+	public ContactBook() {
+		
+	}
+
+	public ArrayList<Contact> getContacts() {
+		return contacts;
+	}
+
+	public boolean add( String name, String email, String phone) {
+		boolean addSuccess = false;
+		Contact contactSearch = search(email);
+		if(contactSearch == null) {
+			contacts.add(new Contact(name, email, phone));
+			addSuccess = true;
+		}
+		return addSuccess;	
+	}
+	
+	public Contact search(String email) {
+	Contact searchContact = null;
+		for(int i = 0;i < contacts.length;i++) {
+			if(contacts.get(i).getEmail().equals(email)) {
+				searchContact = contacts.get(i);
+			}
+		}
+	return searchContact;
+	}
+	
 	
 }
